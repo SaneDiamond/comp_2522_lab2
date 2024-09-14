cb04b61088d706f0ac910094cf2b63aa4ae9db06
MarcusLages <marcusvlages@gmail.com>
2024-09-13 18:03:36 -0700

    Marcus: Reviewed Dragon.java

--- src/code/ca/bcit2522/Lab2/BAM/Dragon.java
+++ src/code/ca/bcit2522/Lab2/BAM/Dragon.java
@@ -2,7 +2,22 @@ package ca.bcit2522.Lab2.BAM;
 
 import java.util.Random;
 
+// Marcus:
+// - Check curly braces and format all over
 public class Dragon extends Creature {
+    // Marcus:
+    // - Common organization of a program:
+    //   1. Constants
+    //   2. Instance variables
+    //   3. Constructors
+    //   4. Rest of the methods
+    //   5. Helper methods
+
+    // Marcus:
+    // - Calling a method outside of a method/constructor is very bad practice.
+    //   Create rand here, then instantiate it in a constructor or instance initializer.
+    // - You should prob give a better name to rand as well, like randNumGenerator or
+    //   something like that
     Random rand = new Random();
 
     private static final int EMPTY = 0;
@@ -13,10 +28,19 @@ public class Dragon extends Creature {
     private final String name;
     private final int health;
     private final Date dateOfBirth;
+    // Marcus:
+    // - Calling a method outside of a method/constructor is very bad practice.
+    //   Create rand here, then instantiate it in a constructor or instance initializer.
     private int firePower = rand.nextInt(100);
 
-
+    // Marcus:
+    // - Don't forget to put arguments as final
+    // - Names of methods should start with lowercase (example: methodName())
     private static void DragonValidation(String name, Date dateOfBirth, int health, int firePower){
+        // Marcus:
+        // - Name, health and date of birth are already validated in creature
+        // - It's only necessary to validate fire power (don't forget to print why the
+        //   error happened and print the value of firePower)
         if (name == null ||
                 name.isEmpty() ||
                 dateOfBirth == null ||
@@ -26,7 +50,10 @@ public class Dragon extends Creature {
             throw new IllegalArgumentException("invalid input");
         }
 
-    }    //Constructor with validation
+    }
+
+    // Marcus: missing final for arguments
+    //Constructor with validation
     public Dragon(String name, Date dateOfBirth, int health, int firePower){
         super(name, dateOfBirth, health);
         // Validation
@@ -56,7 +83,10 @@ public class Dragon extends Creature {
     }
 
 
-
+    // Marcus:
+    // - Don't forget to instatiate and initialize in different lines and
+    //   put String str as final
+    // - If you can, you can use StringBuilder
     @Override
     public String getDetails() {
         String str = String.format("Name:" + name + "Date Of Birth:" +
@@ -65,6 +95,10 @@ public class Dragon extends Creature {
         return str;
     }
 
+    // Marcus:
+    // - In this method, if firePower is less than the minimum it should throw
+    //   an error, not try to restore fire power. Then you add "throws" and the
+    //   name of the exception to the function name
     public void breatheFire(){
         if (firePower < MIN_FIRE_POWER) {
             try
@@ -74,16 +108,20 @@ public class Dragon extends Creature {
             catch (Exception e){
                 System.out.println("invalid fire power amount");
             }
+            // Marcus:
+            // - This braces are not linked to anything.
             {
                 firePower -= MIN_FIRE_POWER;
                 System.out.println("20 DAMAGE DEALT");
             }
         }}
 
+    // Marcus: missing final for argument
     public void restoreFirePower(int amount) {
         if (amount < EMPTY || amount > MAX_FIRE_POWER) {
             throw new IllegalArgumentException("Restored amount is invalid");
         }
+        // Marcus: You already have a random object, you don't need this.
         Random rand = new Random();
         firePower = rand.nextInt(amount);
         System.out.println("Restored fire power to " + firePower);
