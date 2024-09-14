454373b577504a84b5c366ad350e3fb30daf7a58
MarcusLages <marcusvlages@gmail.com>
2024-09-14 11:21:41 -0700

    Commented Orc.java

--- src/code/ca/bcit2522/Lab2/BAM/Orc.java
+++ src/code/ca/bcit2522/Lab2/BAM/Orc.java
@@ -1,14 +1,13 @@
 package ca.bcit2522.Lab2.BAM;
 
 /**
- *  Represents Orc, a subclass that extends Creature, adding berserk as a way to deal damage and rage, which doubles
- *  the berserk damage dealt.
+ * Represents Orc, a subclass that extends Creature, adding berserk as a way to deal damage and rage, which doubles
+ * the berserk damage dealt.
  *
- * @author Andre
+ * @author Ben, Andre, Marcus
  * @version 1.0
  */
-public class Orc extends Creature
-{
+public class Orc extends Creature {
     private static final int MIN_RAGE = 5;
     private static final int MAX_RAGE = 30;
     private static final int NO_RAGE = 0;
@@ -20,22 +19,14 @@ public class Orc extends Creature
     private final int rage;
 
     // Marcus:
-    // - Common organization of a program:
-    //   1. Constants
-    //   2. Instance variables
-    //   3. Constructors
-    //   4. Rest of the methods
-    //   5. Helper methods
-
-    // Marcus:
-    // - Add final to the argument
+    // - Missing constructor description
+    // - Missing @throws description on JavaDoc
 
     /**
-     *
-     * @param name is the name of the orc.
-     * @param dateOfBirth is the birthdate of the orc.
+     * @param name         is the name of the orc.
+     * @param dateOfBirth  is the birthdate of the orc.
      * @param healthPoints represents the health points of the orc.
-     * @param rage represents a buff. When doubled, berserk deals double the damage.
+     * @param rage         represents a buff. When doubled, berserk deals double the damage.
      */
     public Orc(final String name, final Date dateOfBirth, final int healthPoints, final int rage) {
         super(name, dateOfBirth, healthPoints);
@@ -43,12 +34,8 @@ public class Orc extends Creature
         this.rage = rage;
     }
 
-
-    // Marcus:
-    // - It would be good if you could use StringBuilder
     @Override
-    public String getDetails()
-    {
+    public String getDetails() {
         final StringBuilder detailsBuilder;
         final String details;
         final String superDetails;
@@ -65,35 +52,29 @@ public class Orc extends Creature
     }
 
     // Marcus:
-    // - Besides dealing damage, the logic is wrong, because the max rage is 30
-    //   while the ceiling for applying the 2x dmg is 20
     // - Add final to the argument
     public void berserk(int rage, final Creature victim) {
         rage += MIN_RAGE;
-        if (rage >= RAGE_DOUBLE_DAMAGE && rage <= MAX_RAGE)
-        {
+        if (rage >= RAGE_DOUBLE_DAMAGE && rage <= MAX_RAGE) {
             System.out.println("NOW DEALING DOUBLE THE DAMAGE! RAGE IS " + rage);
         }
-        if (rage >= MAX_RAGE)
-        {
+        if (rage >= MAX_RAGE) {
             rage = MAX_RAGE;
             System.out.println("NOW DEALING DOUBLE THE DAMAGE! RAGE IS " + rage);
         } else if (rage < MIN_RAGE) {
             throw new LowRageException("Rage is too low");
         }
-        if (rage >= RAGE_DOUBLE_DAMAGE)
-        {
+        if (rage >= RAGE_DOUBLE_DAMAGE) {
             victim.takeDamage(BERSERK_DAMAGE_DOUBLED);
-        }else
-        {
-            victim.takeDamage (BERSERK_DAMAGE);
+        } else {
+            victim.takeDamage(BERSERK_DAMAGE);
         }
     }
 
     // Marcus:
-    // - Magic number
+    // - No need to JavaDoc private stuff, but still needs a comment description
+
     /**
-     *
      * @param rage when at 20, orc deals double damage.
      * @throws IllegalArgumentException if rage is below 5.
      */
