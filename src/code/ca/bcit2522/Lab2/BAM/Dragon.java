312549f7b605dba1ba6b514caf05056c85ddd00a
Ben Nguyen <benc.nguyen02@gmail.com>
2024-09-14 11:32:36 -0700

    made changes marcus san commented

--- src/code/ca/bcit2522/Lab2/BAM/Dragon.java
+++ src/code/ca/bcit2522/Lab2/BAM/Dragon.java
@@ -14,11 +14,10 @@ public class Dragon extends Creature {
     //   5. Helper methods
 
     // Marcus:
-    // - Calling a method outside of a method/constructor is very bad practice.
+    // - Calling a method outside a method/constructor is very bad practice.// oopsie woopsie
     //   Create rand here, then instantiate it in a constructor or instance initializer.
     // - You should prob give a better name to rand as well, like randNumGenerator or
     //   something like that
-    Random rand = new Random();
 
     private static final int EMPTY = 0;
     private static final int MAX_FIRE_POWER = 100;
@@ -31,38 +30,38 @@ public class Dragon extends Creature {
     // Marcus:
     // - Calling a method outside of a method/constructor is very bad practice.
     //   Create rand here, then instantiate it in a constructor or instance initializer.
-    private int firePower = rand.nextInt(100);
+    Random randNumGenerator = new Random();
+    private int firePower = randNumGenerator.nextInt(100);
 
     // Marcus:
-    // - Don't forget to put arguments as final
-    // - Names of methods should start with lowercase (example: methodName())
-    private static void DragonValidation(String name, Date dateOfBirth, int health, int firePower){
+    // - Don't forget to put arguments as final // yes mastah
+    // - Names of methods should start with lowercase (example: methodName()) // ok mastah
+
+    private static void dragonValidation(final String name, final Date dateOfBirth, final int health, final int firePower) {
         // Marcus:
         // - Name, health and date of birth are already validated in creature
         // - It's only necessary to validate fire power (don't forget to print why the
-        //   error happened and print the value of firePower)
-        if (name == null ||
-                name.isEmpty() ||
-                dateOfBirth == null ||
-                health <= EMPTY ||
-                firePower < EMPTY ||
-                firePower > MAX_FIRE_POWER) {
-            throw new IllegalArgumentException("invalid input");
+        //   error happened and print the value of firePower) // okie dokie master
+        if (firePower < EMPTY || firePower > MAX_FIRE_POWER) {
+            // Print error message and firePower value
+            System.out.println("Invalid firePower: " + firePower);
+            throw new IllegalArgumentException("Fire power is out of bounds: " + firePower);
         }
-
     }
 
-    // Marcus: missing final for arguments
-    //Constructor with validation
-    public Dragon(String name, Date dateOfBirth, int health, int firePower){
+
+    // Marcus: missing final for arguments // check
+    // Constructor with validation
+    public Dragon(final String name, final Date dateOfBirth,final int health, final int firePower){
         super(name, dateOfBirth, health);
         // Validation
-        DragonValidation(name, dateOfBirth, health, firePower);
+        dragonValidation(name, dateOfBirth, health, firePower);
 
         this.name = name;
         this.dateOfBirth = dateOfBirth;
         this.health = health;
         this.firePower = firePower;
+        this.randNumGenerator = new Random();
 
     }
 
@@ -85,47 +84,48 @@ public class Dragon extends Creature {
 
     // Marcus:
     // - Don't forget to instatiate and initialize in different lines and
-    //   put String str as final
-    // - If you can, you can use StringBuilder
+    //   put String str as final //check
+    // - If you can, you can use StringBuilder // okie dokie
     @Override
     public String getDetails() {
-        String str = String.format("Name:" + name + "Date Of Birth:" +
-                dateOfBirth + "Health:" + health + "FirePower:" + firePower);
+        StringBuilder str = new StringBuilder();
+        str.append("Name:").append(name)
+                .append(" Date Of Birth:").append(dateOfBirth)
+                .append(" Health:").append(health)
+                .append(" FirePower:").append(firePower);
 
-        return str;
+        return str.toString();
     }
 
     // Marcus:
     // - In this method, if firePower is less than the minimum it should throw
     //   an error, not try to restore fire power. Then you add "throws" and the
-    //   name of the exception to the function name
+    //   name of the exception to the function name // check prolly
     // - Add damage to a creature through argument (you know it, but just so no
-    //   one forgets it
-    public void breatheFire(){
+    //   one forgets it // check
+    public void breatheFire(Creature creature) throws LowFirePowerException {
         if (firePower < MIN_FIRE_POWER) {
-            try
-            {
-                restoreFirePower(INVALID_AMOUNT);
-            }
-            catch (Exception e){
-                System.out.println("invalid fire power amount");
-            }
-            // Marcus:
-            // - This braces are not linked to anything.
-            {
-                firePower -= MIN_FIRE_POWER;
-                System.out.println("20 DAMAGE DEALT");
-            }
-        }}
+            // Throwing the custom LowFirePowerException
+            throw new LowFirePowerException("Fire power is too low to breathe fire.");
+        } else {
+            // Reducing firePower and applying damage
+            firePower -= MIN_FIRE_POWER;
+            int damage = 20;
+            creature.takeDamage(damage);
+            System.out.println(damage + " DAMAGE DEALT");
+        }
+    }
 
-    // Marcus: missing final for argument
-    public void restoreFirePower(int amount) {
+
+
+    // Marcus: missing final for argument // yes mastah
+    public void restoreFirePower(final int amount) {
         if (amount < EMPTY || amount > MAX_FIRE_POWER) {
             throw new IllegalArgumentException("Restored amount is invalid");
         }
-        // Marcus: You already have a random object, you don't need this.
+        // Marcus: You already have a random object, you don't need this. // ok sir mb
         Random rand = new Random();
-        firePower = rand.nextInt(amount);
+        firePower = randNumGenerator.nextInt(amount);
         System.out.println("Restored fire power to " + firePower);
     }
 
--- src/code/ca/bcit2522/Lab2/BAM/LowFirePowerException.java
+++ src/code/ca/bcit2522/Lab2/BAM/LowFirePowerException.java
@@ -1,7 +1,8 @@
 package ca.bcit2522.Lab2.BAM;
 
-class LowFirePowerException extends Exception {
+public class LowFirePowerException extends Exception {
     public LowFirePowerException(String message) {
-        super("NO FIRE POWER");
+        super(message);
     }
 }
+
