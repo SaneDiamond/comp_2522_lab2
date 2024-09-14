7834e29c6f6d48dd2902605c9a1adacb9d7f1258
MarcusLages <marcusvlages@gmail.com>
2024-09-13 18:03:47 -0700

    Marcus: Reviewed Orc.java

--- src/code/ca/bcit2522/Lab2/BAM/Orc.java
+++ src/code/ca/bcit2522/Lab2/BAM/Orc.java
@@ -3,6 +3,16 @@ package ca.bcit2522.Lab2.BAM;
 public class Orc extends Creature {
     private int rage;
 
+    // Marcus:
+    // - Common organization of a program:
+    //   1. Constants
+    //   2. Instance variables
+    //   3. Constructors
+    //   4. Rest of the methods
+    //   5. Helper methods
+
+    // Marcus:
+    // - Add final to the argument
     public Orc(String name, Date dateOfBirth, int healthPoints, int rage) {
         super(name, dateOfBirth, healthPoints);
         validateRage(rage);
@@ -12,11 +22,17 @@ public class Orc extends Creature {
     private static final int MIN_RAGE = 5;
     private static final int MAX_RAGE = 20;
 
+    // Marcus:
+    // - It would be good if you could use StringBuilder
     @Override
     public String getDetails() {
         return String.format("%s, Rage: %d", super.getDetails(), rage);
     }
 
+    // Marcus:
+    // - Besides dealing damage, the logic is wrong, because the max rage is 30
+    //   while the ceiling for applying the 2x dmg is 20
+    // - Add final to the argument
     public void berserk(int rage) {
         rage += MIN_RAGE;
         if (rage >= MAX_RAGE) {
@@ -27,6 +43,8 @@ public class Orc extends Creature {
         }
     }
 
+    // Marcus:
+    // - Magic number
     private static void validateRage(final int rage) {
         if (rage < 0) {
             throw new IllegalArgumentException("Rage cannot be negative.");
