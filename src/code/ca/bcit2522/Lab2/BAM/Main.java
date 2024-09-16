35487466ba527b8c8671fe25b46fb1d631675698
SaneDiamond <andredizon3@gmail.com>
2024-09-16 11:45:15 -0700

    completed the battle scene

--- src/code/ca/bcit2522/Lab2/BAM/Main.java
+++ src/code/ca/bcit2522/Lab2/BAM/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
         Date dragonDate = new Date(2010, 10, 10);
-        Creature dragon = new Dragon("Hanoor", dragonDate, 100, 80);
+        Creature dragon = new Dragon("Hanoor", dragonDate, 300, 80);
 //        System.out.println(dragon.getDetails());
 
         Date elfDate = new Date(2008, 8, 8);
@@ -43,41 +43,88 @@ public class Main {
 
 
         }
+
+        System.out.println("Pajeet attacks Thrall!");
         try {
             elfPajeet.castSpell(orcThrall);
+            System.out.println("Thrall's health is now " + orcThrall.getHealthPoints());
         } catch (LowManaException e) {
             System.out.println(e.getMessage());
         }
 
+        System.out.println("\nThrall attacks Pajeet!" );
         try {
             orcThrall.berserk(elfPajeet);
+            System.out.println("Pajeet's health is now " + elfPajeet.getHealthPoints());
         } catch (LowRageException e) {
             System.out.println(e.getMessage());
         }
 
+        System.out.println("\nHanoor attacks Thrall!");
         try {
             dragonHanoor.breatheFire(orcThrall);
+            System.out.println("Thrall's health is now " + orcThrall.getHealthPoints());
         } catch (LowFirePowerException e) {
             System.out.println(e.getMessage());
         }
 
+        System.out.println("\nHanoor attacks Pajeet!");
+        try {
+            dragonHanoor.breatheFire(elfPajeet);
+            System.out.println("Pajeet's health is now " + elfPajeet.getHealthPoints());
+        }catch (LowFirePowerException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\nThrall attacks Hanoor!");
         try {
             orcThrall.berserk(dragonHanoor);
+            System.out.println("Hanoor's health is now " + dragonHanoor.getHealthPoints());
         } catch (LowRageException e) {
             System.out.println(e.getMessage());
         }
 
+        System.out.println("\nThrall attacks Hanoor!");
         try {
             orcThrall.berserk(dragonHanoor);
+            System.out.println("Hanoor's health is now " + dragonHanoor.getHealthPoints());
         } catch (LowRageException e) {
             System.out.println(e.getMessage());
         }
 
+        System.out.println("\nPajeet attacks Hanoor!");
         try {
-            orcThrall.berserk(dragonHanoor);
-        } catch (LowRageException e) {
+            elfPajeet.castSpell(dragonHanoor);
+            System.out.println("Hanoor's health is now " + dragonHanoor.getHealthPoints());
+        } catch (LowManaException e) {
             System.out.println(e.getMessage());
         }
 
+        System.out.println("\nPajeet attacks Hanoor!");
+        try {
+            elfPajeet.castSpell(dragonHanoor);
+            System.out.println("Hanoor's health is now " + dragonHanoor.getHealthPoints());
+        } catch (LowManaException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\nHanoor attacks Thrall!");
+        try {
+            dragonHanoor.breatheFire(orcThrall);
+            System.out.println("Thrall's health is now " + orcThrall.getHealthPoints());
+        } catch (LowFirePowerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\nHanoor attacks Pajeet!");
+        try {
+            dragonHanoor.breatheFire(elfPajeet);
+            System.out.println("Pajeet's health is now " + elfPajeet.getHealthPoints());
+        }catch (LowFirePowerException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\nTheir final health is" + "\nThrall: " + orcThrall.getHealthPoints() + "\nHanoor: "
+                + dragonHanoor.getHealthPoints() + "\nPajeet: " + elfPajeet.getHealthPoints());
     }
 }
