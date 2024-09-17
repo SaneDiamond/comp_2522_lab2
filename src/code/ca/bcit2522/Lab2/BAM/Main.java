b119fc7db481cc8c8b734f79a14b3c5a8f07ec72
Ben Nguyen <benc.nguyen02@gmail.com>
2024-09-17 12:37:50 -0700

    HEALLLING

--- src/code/ca/bcit2522/Lab2/BAM/Main.java
+++ src/code/ca/bcit2522/Lab2/BAM/Main.java
@@ -5,14 +5,24 @@ public class Main {
         // Marcus:
         // - Don't forget to delete debug comments
         Date dragonDate = new Date(2010, 10, 10);
-        Creature dragon = new Dragon("Hanoor", dragonDate, 300, 80);
-//        System.out.println(dragon.getDetails());
+        Creature dragon = new Dragon("Harnoor", dragonDate, 300, 80);
+        System.out.println(dragon.getDetails());
+        System.out.println("Class detected using getClass(): " + dragon.getClass().getSimpleName());
+        System.out.println(dragon instanceof Creature ? "This is a Dragon, checked using instanceof\n" : "This is not a Dragon");
 
         Date elfDate = new Date(2008, 8, 8);
         Creature elf = new Elf("Pajeet", elfDate, 100, 50);
-//        System.out.println(elf.getDetails());
+        System.out.println(elf.getDetails());
+        System.out.println("Class detected using getClass(): " + elf.getClass().getSimpleName());
+        System.out.println(elf instanceof Creature ? "This is an elf, checked using instanceof\n" : "This is not a Dragon");
+
         Date orcDate = new Date(2000, 4, 20);
         Creature orc = new Orc("Thrall", orcDate, 120, 5);
+        System.out.println(orc.getDetails());
+        System.out.println("Class detected using getClass(): " + orc.getClass().getSimpleName());
+        System.out.println(orc instanceof Creature ? "This is a orc, checked using instanceof\n" : "This is not a Dragon");
+
+
 
         Orc orcThrall = new Orc("Thrall", orcDate, 120, 5);
         Elf elfPajeet = new Elf("Pajeet", elfDate, 100, 50);
@@ -24,30 +34,22 @@ public class Main {
         //   (I know, it's dumb, but I don't wanna get punched by a bald guy)
         // bruh what da hell bruh
         // store creatures in array
-        Creature[] creatures = {dragon, elf, orc};
-        // loop through creatures and get details of each
-        for (Creature creature : creatures) {
-            // Marcus:
-            // - 2 curly braces unnecessary
-            {
-                System.out.println(creature.getDetails());
-
-                // Using instance loop of to check class
-                switch (creature) {
-                    // Marcus:
-                    // - Don't use those quick switch cases
-                    case Dragon dragon1 -> System.out.println("This is a Dragon, checked with using instanceof");
-                    case Elf elf1 -> System.out.println("This is an Elf, checked with using instanceof");
-                    case Orc orc1 -> System.out.println("This is an Orc, checked with using instanceof");
-                    default -> {
-                    }
-                }
-                // Check class using the get class method to also check class
-                System.out.println("Class detected using getClass(): " + creature.getClass().getSimpleName() + "\n");
-            }
-
-
-        }
+//        Creature[] creatures = {dragon, elf, orc};
+//        // loop through creatures and get details of each
+//        for (Creature creature : creatures) {
+//            // Marcus:
+//            // - 2 curly braces unnecessary // oki doki
+//                // Using instance loop of to check class
+//                switch (creature) {
+//                    // Marcus:
+//                    // - Don't use those quick switch cases
+//                    case Dragon dragon1 -> System.out.println("This is a Dragon, checked with using instanceof");
+//                    case Elf elf1 -> System.out.println("This is an Elf, checked with using instanceof");
+//                    case Orc orc1 -> System.out.println("This is an Orc, checked with using instanceof");
+//                    default -> {
+//                    }
+//                }
+//        }
 
         // Marcus:
         // - Use the name of the object instead of just "Pajeet"
@@ -123,6 +125,12 @@ public class Main {
             elfPajeet.castSpell(dragonHanoor);
             System.out.println("Hanoor's health is now " + dragonHanoor.getHealthPoints());
         } catch (LowManaException e) {
+            System.out.println(e.getMessage());
+        }
+            dragonHanoor.heal(5);
+            System.out.println(dragon.getName() + "Heals himself");
+        try {
+        } catch (HealingException e) {
             System.out.println(e.getMessage());
         }
 
