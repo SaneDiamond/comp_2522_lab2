7fdd3295a2b5dd6851b6f5ded9c90b32b9b1fcc5
MarcusLages <marcusvlages@gmail.com>
2024-09-18 17:35:34 -0700

    Fixed Main.java

--- src/code/ca/bcit2522/Lab2/BAM/Main.java
+++ src/code/ca/bcit2522/Lab2/BAM/Main.java
@@ -1,7 +1,5 @@
 package ca.bcit2522.Lab2.BAM;
 
-
-
 /**
  * Main class used to test superclass Creature and its subclasses: CreatureHealer,
  * Date, Dragon, Elf, and Orc.
@@ -24,31 +22,75 @@ public class Main {
         Creature dragon = new Dragon("Smaug", dragonDate, 300, 80);
         System.out.println(dragon.getDetails());
         System.out.println("Class detected using getClass(): " + dragon.getClass().getSimpleName());
-        System.out.println(dragon instanceof Creature ? "This is a dragon, checked using instanceof\n" : "This is not a dragon");
+        if (dragon instanceof Dragon){
+            System.out.println("This is a dragon, checked using instanceof");
+        } else {
+            System.out.println("This is not a dragon.");
+        }
 
         Date elfDate = new Date(2008, 8, 8);
         Creature elf = new Elf("Legolas", elfDate, 100, 50);
         System.out.println(elf.getDetails());
         System.out.println("Class detected using getClass(): " + elf.getClass().getSimpleName());
-        System.out.println(elf instanceof Creature ? "This is an elf, checked using instanceof\n" : "This is not an elf");
+        if (elf instanceof Elf) {
+            System.out.println("This is an elf, checked using instanceof");
+        } else {
+            System.out.println("This is not an elf.");
+        }
 
         Date orcDate = new Date(2000, 4, 20);
         Creature orc = new Orc("Thrall", orcDate, 120, 5);
         System.out.println(orc.getDetails());
         System.out.println("Class detected using getClass(): " + orc.getClass().getSimpleName());
-        System.out.println(orc instanceof Creature ? "This is a orc, checked using instanceof\n" : "This is not an orc");
-
+        if (orc instanceof Orc) {
+            System.out.println("This is an orc, checked using instanceof");
+        } else {
+            System.out.println("This is not an orc.");
+        }
         Date healerDate = new Date(2001, 11, 9);
         Creature healer = new CreatureHealer("Mercy", healerDate, 60);
         System.out.println(healer.getDetails());
         System.out.println("Class detected using getClass(): " + healer.getClass().getSimpleName());
-        System.out.println(healer instanceof Creature ? "This is a healer, checked using instanceof\n" : "This is not a healer");
+        if (healer instanceof CreatureHealer){
+            System.out.println("This is a healer, checked using instanceof");
+        }else {
+            System.out.println("This is not a healer.");
+        }
 
-        Dragon dragonSmaug = new Dragon("Smaug", dragonDate, 100, 80);
-        Orc orcThrall = new Orc("Thrall", orcDate, 120, 5);
-        Elf elfLegolas = new Elf("Legolas", elfDate, 100, 50);
-        CreatureHealer healerMercy = new CreatureHealer("Mercy", healerDate, 60);
 
+        Dragon dragonSmaug;
+        Orc orcThrall;
+        Elf elfLegolas;
+        CreatureHealer healerMercy;
+
+        dragonSmaug = null;
+        orcThrall = null;
+        elfLegolas = null;
+        healerMercy = null;
+
+        if (dragon instanceof Dragon) {
+            dragonSmaug = (Dragon) dragon;
+        } else {
+            System.out.println("This is not a dragon.");
+        }
+
+        if (orc instanceof Orc) {
+            orcThrall = (Orc) orc;
+        } else {
+            System.out.println("This is not an orc.");
+        }
+
+        if (elf instanceof Elf){
+            elfLegolas =  (Elf) elf;
+        } else {
+            System.out.println("This is not an elf.");
+        }
+
+        if (healer instanceof CreatureHealer){
+            healerMercy = (CreatureHealer) healer;
+        } else {
+            System.out.println("This is not a healer.");
+        }
 
         System.out.println(elfLegolas.getName() + " attacks " + orcThrall.getName() + "!");
         try {
@@ -67,7 +109,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("");
+        System.out.println();
         try {
             orcThrall.heal(HEAL_AMOUNT);
             System.out.println(orcThrall.getName() + " heals himself");
@@ -76,7 +118,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("");
+        System.out.println();
         System.out.println(dragonSmaug.getName() + " attacks " + orcThrall.getName() + "!");
         try {
             dragonSmaug.breatheFire(orcThrall);
@@ -95,7 +137,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("");
+        System.out.println();
         System.out.println(orcThrall.getName() + " attacks " + dragonSmaug.getName() + "1!");
         try {
             orcThrall.berserk(dragonSmaug);
@@ -104,8 +146,8 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("");
-        System.out.println(orcThrall.getName() + " attacks " + dragonSmaug.getName() + "1!");
+        System.out.println();
+        System.out.println(orcThrall.getName() + " attacks " + dragonSmaug.getName() + "!");
         try {
             orcThrall.berserk(dragonSmaug);
             System.out.println(dragonSmaug.getName() + "'s health is now " + dragonSmaug.getHealthPoints());
@@ -113,7 +155,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("");
+        System.out.println();
         System.out.println(elfLegolas.getName() + " attacks " + dragonSmaug.getName() + "!");
         try {
             elfLegolas.castSpell(dragonSmaug);
@@ -122,7 +164,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("");
+        System.out.println();
         try {
             dragonSmaug.heal(HEAL_AMOUNT);
             System.out.println(dragonSmaug.getName() + " heals himself");
@@ -133,7 +175,7 @@ public class Main {
         }
 
 
-        System.out.println("");
+        System.out.println();
         System.out.println(elfLegolas.getName() + " attacks " + dragonSmaug.getName() + "!");
         try {
             elfLegolas.castSpell(dragonSmaug);
@@ -143,7 +185,7 @@ public class Main {
         }
 
 
-        System.out.println("");
+        System.out.println();
         System.out.println(dragonSmaug.getName() + " attacks " + orcThrall.getName() + "!");
         try {
             dragonSmaug.breatheFire(orcThrall);
@@ -153,7 +195,7 @@ public class Main {
         }
 
 
-        System.out.println("");
+        System.out.println();
         System.out.println(dragonSmaug.getName() + " attacks " + elfLegolas.getName() + "!");
         try {
             dragonSmaug.breatheFire(elfLegolas);
@@ -163,7 +205,7 @@ public class Main {
         }
 
 
-        System.out.println("");
+        System.out.println();
         System.out.println(healerMercy.getName() + " heals " + dragonSmaug.getName() + "!");
         try
         {
@@ -175,7 +217,7 @@ public class Main {
         }
 
 
-        System.out.println("");
+        System.out.println();
         System.out.println(healerMercy.getName() + " heals " + dragonSmaug.getName() + "!");
         try
         {
@@ -186,8 +228,19 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
+        System.out.println();
+        System.out.println(orcThrall.getName() + " attacks " + healerMercy.getName() + "!");
+        try {
+            orcThrall.berserk(healerMercy);
+            System.out.println(healerMercy.getName() + "'s health is now " + healerMercy.getHealthPoints());
+        } catch (LowRageException e) {
+            System.out.println(e.getMessage());
+        }
 
-        System.out.println("\nTheir final health is" + "\nThrall: " + orcThrall.getHealthPoints() + "\nHanoor: "
-                + dragonSmaug.getHealthPoints() + "\nPajeet: " + elfLegolas.getHealthPoints());
+
+        System.out.println("\nTheir final health is" + "\n" + orcThrall.getName() + ": " + orcThrall.getHealthPoints()
+                + "\n" + dragonSmaug.getName() + ": " + dragonSmaug.getHealthPoints()
+                + "\n" + elfLegolas.getName() + ": " + elfLegolas.getHealthPoints()
+                + "\n" + healerMercy.getName() + ": " + healerMercy.getHealthPoints());
     }
 }
